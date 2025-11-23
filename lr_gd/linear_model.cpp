5e0b9f12531f144a11038783f397893b9ef73c25
theadamsabra <theadamsabra@gmail.com>
2025-11-22 16:11:49 -0800

    get working training data generation

--- lr_gd/linear_model.cpp
+++ lr_gd/linear_model.cpp
@@ -1,12 +1,14 @@
 #include <vector>
+#include <iostream>
+#include <random>
 using namespace std;
 
-class LinearModel
+class SimpleLinearModel
 {
     private:
     // Define weights and biases:
-    vector<double> W;
-    vector<double> b;
+    double W = rand();
+    double b = rand();
 
     public:
 
@@ -16,22 +18,46 @@ class LinearModel
     };
 };
 
-tuple < vector<double>, vector<double> > generate_training_data(){
-    // TODO: Properly generate random numbers (ideally betweeen 0 and 1)
-    vector<double> x_train = generate(0, 1000);
+pair <vector<double>, vector<double>>  generate_training_data(int N, int true_W, int true_b){
+    /*
+    Simulate random training data for the Linear Model: 
+    
+    @param N number of samples to randomly generate
+    @param true_W true weight of the linear model to be predicted 
+    @param true_b true bias of the linear model to be predicted
+    */
 
-    // TODO: Update this to loop:
-    vector<double> y_train = 5*x_train + 22;
+    // Generate random device, set seed, and generate
+    // a random gaussian:
+    mt19937 generator(36);
+    normal_distribution<double> standard_gaussian(0, 1);
 
-    return make_tuple(x_train, y_train);
+    // Generate dummy x_train data: 
+    vector<double> x_train(N), y_train(N);
+
+    // Construct the data: 
+    for (int i=0; i<N; i++){
+        double random_number = standard_gaussian(generator);
+        x_train[i] = random_number;
+        y_train[i] = (true_W * random_number) + true_b;
+    }
+
+    return make_pair(x_train, y_train);
 };
 
 // Main function to test the linear model:
 int main(){
+    // Params for training:
     int num_epochs = 10;
     float lr = 0.01;
 
-    vector<double> x_train, y_train = generate_training_data();
-    LinearModel lm = LinearModel();
-    lm.train(x_train, y_train, num_epochs, lr);
+    // This can be really whatever
+    int N = 10000;
+    int true_W = 5;
+    int true_b = 22;
+
+    // This is C++ 17.
+    // Guess I'm using this for now
+    auto [x_train, y_train] = generate_training_data(N, true_W, true_b);
+    return 0;
 }
