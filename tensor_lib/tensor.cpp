ede1247ee26ddc5e5d2475bb5cc588e6f841ac5d
theadamsabra <theadamsabra@gmail.com>
2025-11-28 17:53:34 -0800

    overload multiplication

--- tensor_lib/tensor.cpp
+++ tensor_lib/tensor.cpp
@@ -104,6 +104,26 @@ public:
         };
         return output;
     };
+
+    // Overload multiplication 
+    Tensor<T&> operator*(Tensor<T&> t){
+        // Verify shape is equal:
+        Shape self_shape = shape; 
+        Shape additive_shape = t.get_shape();
+        assert(self_shape == additive_shape);
+
+        // Verify rank is equal:
+        size_t n = self_shape.size();
+        size_t m = additive_shape.size();
+        assert(n == m);
+
+        Tensor<T> output(self_shape);
+        // Add by simple indexing across entire vector:
+        for (int i=0; i<data.size(); i++){
+            output[i] = data[i] * additive_shape[i];
+        };
+        return output;
+    };
 };
 
 int main(){
